8dd93147ba4bcf0883640ff7afee02d953d36c90
Ouutou <mail@uestc.icu>
2020-12-31 09:48:40 +0800

    1231学习GUI

--- gUI/hitme.py
+++ gUI/hitme.py
@@ -1,24 +1,24 @@
 import tkinter as tk
+import random
 
 mainWindow = tk.Tk()
 mainWindow.title("Hit Me!")
 mainWindow.geometry("1280x720")
 varText = tk.StringVar()
 varText.set("Hello! Anybody there?")
-s = tk.Label(textvariable=varText, bg="#66ccff")
+s = tk.Label(textvariable=varText, bg="#66ccff", font=("方正楷体_GBK", 14))
 s.pack()
-
-doYouHit = 0
 
 
 def hit_me():
-    global doYouHit, varText
-    if doYouHit == 0:
-        doYouHit = 1
-        varText.set("Oh! That's hurt!")
-    else:
-        doYouHit = 0
-        varText.set("Stop! I know you are here!")
+    global varText
+    do_you_hit = random.randint(1, 3)
+    if do_you_hit == 1:
+        varText.set("1. Oh! That's hurt!")
+    elif do_you_hit == 2:
+        varText.set("2. Stop! I know you are here!")
+    elif do_you_hit == 3:
+        varText.set("3. FUCK YOU!!!")
 
 
 button = tk.Button(mainWindow,
